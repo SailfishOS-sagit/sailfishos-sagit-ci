8e6f1330384d0525e8cb65107d55704b71cb4ce3
BirdZhang <0312birdzhang@gmail.com>
2019-12-10 14:00:35 +0800

    添加回tmate

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 source hadk.env
+
+mkdir -p $ANDROID_ROOT
 cd $ANDROID_ROOT
 
 export PLATFORM_SDK_ROOT=/srv/mer
-curl -k -O http://releases.sailfishos.org/sdk/installers/latest/Jolla-latest-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2 ;
-sudo mkdir -p $PLATFORM_SDK_ROOT/sdks/sfossdk ;
-sudo tar --numeric-owner -p -xjf Jolla-latest-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2 -C $PLATFORM_SDK_ROOT/sdks/sfossdk  ;
+curl -k -O http://releases.sailfishos.org/sdk/installers/latest/Jolla-latest-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2
+sudo mkdir -p $PLATFORM_SDK_ROOT/sdks/sfossdk
+sudo tar --numeric-owner -p -xjf Jolla-latest-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2 -C $PLATFORM_SDK_ROOT/sdks/sfossdk
 echo "export PLATFORM_SDK_ROOT=$PLATFORM_SDK_ROOT" >> ~/.bashrc
-echo 'alias sfossdk=$PLATFORM_SDK_ROOT/sdks/sfossdk/mer-sdk-chroot' >> ~/.bashrc ; exec bash ;
-echo 'PS1="PlatformSDK $PS1"' > ~/.mersdk.profile ;
-echo '[ -d /etc/bash_completion.d ] && for i in /etc/bash_completion.d/*;do . $i;done'  >> ~/.mersdk.profile ;
+echo 'alias sfossdk=$PLATFORM_SDK_ROOT/sdks/sfossdk/mer-sdk-chroot' >> ~/.bashrc
+source ~/.bashrc
+echo 'PS1="PlatformSDK $PS1"' > ~/.mersdk.profile
+echo '[ -d /etc/bash_completion.d ] && for i in /etc/bash_completion.d/*;do . $i;done'  >> ~/.mersdk.profile
 sfossdk
 echo "Entered sfossdk, well done!"
-
-source work/hadk.env
+source ~/work/hadk.env
 cd $ANDROID_ROOT
 sdk-assistant create xiaomi-vince-latest http://releases.sailfishos.org/sdk/targets/Sailfish_OS-latest-Sailfish_SDK_Tooling-i486.tar.7z -y
 sdk-assistant create xiaomi-vince-armv7hl http://releases.sailfishos.org/sdk/targets/Sailfish_OS-latest-Sailfish_SDK_Target-armv7hl.tar.7z -y
