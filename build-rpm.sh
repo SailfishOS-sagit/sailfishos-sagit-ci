eea54d5e3dcf8b41836f1664b25b42b8fbad8e2a
BirdZhang <0312birdzhang@gmail.com>
2020-11-06 10:12:41 +0800

    update to test

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 source /home/nemo/work/ci/ci/hadk.env
+export ANDROID_ROOT=/home/nemo/work/hadk_14.1
 
-mkdir -p $ANDROID_ROOT
+sudo chown -R nemo:nemo $ANDROID_ROOT
 cd $ANDROID_ROOT
 
 cd ~/.scratchbox2
 cp -R SailfishOS-*-armv7hl $VENDOR-$DEVICE-$PORT_ARCH
 cd $VENDOR-$DEVICE-$PORT_ARCH
-sed -i 's/SailfishOS-3.1.0.11/xiaomi-vince/g' sb2.config 
-sudo ln -s /srv/mer/targets/SailfishOS-3.1.0.11-armv7hl /srv/mer/targets/xiaomi-vince-armv7hl
-sudo ln -s /srv/mer/toolings/SailfishOS-3.1.0.11 /srv/mer/toolings/xiaomi-vince
+sed -i 's/SailfishOS-$SAILFISH_VERSION/xiaomi-vince/g' sb2.config 
+sudo ln -s /srv/mer/targets/SailfishOS-$SAILFISH_VERSION-armv7hl /srv/mer/targets/xiaomi-vince-armv7hl
+sudo ln -s /srv/mer/toolings/SailfishOS-$SAILFISH_VERSION /srv/mer/toolings/xiaomi-vince
 
 sdk-assistant list
 
+cd $ANDROID_ROOT
 rpm/dhd/helpers/build_packages.sh --droid-hal
 
 DROIDMEDIA_VERSION=$(git --git-dir external/droidmedia/.git describe --tags | sed \
@@ -25,17 +27,6 @@ sed -ie "s/0.0.0/$DROIDMEDIA_VERSION/" \
 hybris/mw/droidmedia-localbuild/rpm/droidmedia.spec
 mv hybris/mw/droidmedia-$DROIDMEDIA_VERSION.tgz hybris/mw/droidmedia-localbuild
 rpm/dhd/helpers/build_packages.sh --build=hybris/mw/droidmedia-localbuild
-
-AUDIOFLINGERGLUE_VERSION=$(git --git-dir external/audioflingerglue/.git describe --tags | sed \
--r "s/\-/\+/g")
-rpm/dhd/helpers/pack_source_audioflingerglue-localbuild.sh $AUDIOFLINGERGLUE_VERSION
-mkdir -p hybris/mw/audioflingerglue-localbuild/rpm
-cp rpm/dhd/helpers/audioflingerglue-localbuild.spec \
-hybris/mw/audioflingerglue-localbuild/rpm/audioflingerglue.spec
-sed -ie "s/0.0.0/$AUDIOFLINGERGLUE_VERSION/" \
-hybris/mw/audioflingerglue-localbuild/rpm/audioflingerglue.spec
-mv hybris/mw/audioflingerglue-$AUDIOFLINGERGLUE_VERSION.tgz hybris/mw/audioflingerglue-localbuild
-rpm/dhd/helpers/build_packages.sh --build=hybris/mw/audioflingerglue-localbuild
 
 rpm/dhd/helpers/build_packages.sh --droid-hal
 
